553ce929d071d9879d719d8a576f0ca3da76ef4e
skrypnykyuliia <yulscr5@gmail.com>
2024-09-26 15:24:20 +0200

    Первый коммит

---
+++ src/components/ListItems.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+
+const ListItems = () => {
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const addItem = () => {
+    setItems([...items, inputValue]);
+    setInputValue('');
+  };
+
+  useEffect(() => {
+    console.log('Компонент ListItems обновлен');
+  }, [items]);
+  
+  //useEffect(() => {
+  //  console.log('Компонент ListItems обновлен');
+ // }, []); 
+  
+
+  return (
+    <div>
+      <input 
+        type="text" 
+        value={inputValue} 
+        onChange={(e) => setInputValue(e.target.value)} 
+      />
+      <button onClick={addItem}>Добавить элемент</button>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ListItems;
